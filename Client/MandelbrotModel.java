3f1f1b6857c614fd3b2bc795a3d99d448b6899d9
Mateo Thürig <148453207+SidBusak@users.noreply.github.com>
2024-06-09 22:50:04 +0200

    Create MandelbrotModel.java

---
+++ Client/MandelbrotModel.java
@@ -0,0 +1,9 @@
+public class MandelbrotModel {
+    private double zoomX;
+    private double zoomY;
+    private int maxIterations;
+    private int width;
+    private int height;
+
+    // Getter und Setter
+}
