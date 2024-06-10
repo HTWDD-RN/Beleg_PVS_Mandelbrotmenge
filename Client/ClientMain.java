c45b6845a071e4b97973004f54b35a5b99b2a472
Mateo Thürig <148453207+SidBusak@users.noreply.github.com>
2024-06-10 17:23:39 +0200

    Update ClientMain.java

--- Client/ClientMain.java
+++ Client/ClientMain.java
@@ -1,11 +1,21 @@
+import java.rmi.Naming;
+
 public class ClientMain {
     public static void main(String[] args) {
-        MandelbrotModel model = new MandelbrotModel();
-        MandelbrotView view = new MandelbrotViewImpl();
-        MandelbrotService service = (MandelbrotService) Naming.lookup("//localhost/MandelbrotMaster");
-        MandelbrotPresenter presenter = new MandelbrotPresenter(view, model, service);
+        try {
+            MandelbrotModel model = new MandelbrotModel();
+            model.setWidth(1024);
+            model.setHeight(768);
+            model.setMaxIterations(1000);
 
-        // Beispiel für die Eingabe eines Zoompunkts
-        presenter.onZoomPointEntered(-0.34837308755059104, -0.6065038451823017);
+            MandelbrotView view = new MandelbrotViewImpl();
+            MandelbrotService service = (MandelbrotService) Naming.lookup("//localhost/MandelbrotMaster");
+            MandelbrotPresenter presenter = new MandelbrotPresenter(view, model, service);
+
+            // Beispiel für die Eingabe eines Zoompunkts
+            presenter.onZoomPointEntered(-0.34837308755059104, -0.6065038451823017);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
