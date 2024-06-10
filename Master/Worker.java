91b1b4a2301c189e6889964a38efec8d4d0083a5
Mateo Thürig <148453207+SidBusak@users.noreply.github.com>
2024-06-10 17:33:14 +0200

    Update Worker.java

--- Master/Worker.java
+++ Master/Worker.java
@@ -3,5 +3,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Worker extends Remote {
-    BufferedImage computeMandelbrotPart(int startX, int endX, MandelbrotModel model) throws RemoteException;
+    BufferedImage calculateMandelbrotPart(MandelbrotModel model, int startX, int endX) throws RemoteException;
 }
